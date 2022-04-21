6c5591d841bbab2ecb725e8adc5b504b752bc16c
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-21 11:01:51 +0300

    do a program

--- test.cpp
+++ test.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int enterNumber(string text) {
+    int num;
+    cout << text;
+    cin >> num;
+    return num;
+}
+
+string enterString(string text) {
+    string text1;
+    cout << text;
+    cin >> text1;
+    return text1;
+}
+
 int main() {
-    
+    int count = enterNumber("Введіть кількість повторювань: ");
+    string txt = enterString("Введіть слово або рядок який буде повторюватися: ");
+
+    for (int i = 0; i < count; i++) {
+        cout << txt << " " << endl;
+    }
 }
